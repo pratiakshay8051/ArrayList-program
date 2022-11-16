3fb01727a21919675ea0d4e27e6026a6dd4bac2e
pratiakshay8051 <118198866+pratiakshay8051@users.noreply.github.com>
2022-11-16 16:35:30 +0530

    Add files via upload

---
+++ Example_1.java
@@ -0,0 +1,10 @@
+package com.java.demo;
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+
+	System.out.println("i am pratiksha");
+	}
+
+}
---
+++ Example_2.java
@@ -0,0 +1,10 @@
+package com.java.demo;
+
+public class Example_2 {
+
+	public static void main(String[] args) {
+
+	System.out.println("wel come to java programming");
+	}
+
+}
